d25d7668f5a462d120f8a22995272ac4cad74e9f
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-01-16 20:39:40 +0200

    Books/create/index-new

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/book')
 
-router.get('/',(req,res,next)=>{
+router.get('/',async (req,res,next)=>{
+    let books
+    try{
+books = await Book.find().sort({createAt:'desc'}).limit(10).exec()
+    }catch{
+books=[]
+    }
 
-    res.render('index')
+    res.render('index',{books:books})
 
 });
 
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV !== 'production') {
   
   const indexRouter = require('./routes/index')
   const authorRouter = require('./routes/authors')
+  const bookRouter = require('./routes/book')
   
   app.set('view engine', 'ejs')
   app.set('views', __dirname + '/views')
@@ -29,6 +30,7 @@ if (process.env.NODE_ENV !== 'production') {
   
   app.use('/', indexRouter)
   app.use('/authors',authorRouter)
+  app.use('/books',bookRouter)
   
   
   app.listen(process.env.PORT || 3000)
